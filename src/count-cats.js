34b52d6058bded325482cd442620d9cc80b0cfdd
Yurii Senetskyi <yurii_senetskyi@epam.com>
2020-06-02 11:19:36 +0300

    First 5 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  const cat = '^^';
+  for (elem of matrix){
+    let kittensFound = elem.filter(ears => ears===cat).length;
+    if (kittensFound > 0){count = count + kittensFound}
+  }
+  return count
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function convertHoursToSeconds(hours) {
+  return Math.floor(hours * 60 * 60);
+}
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(
+      convertHoursToSeconds(1) * turns / turnsSpeed
+  );
+  return {
+    'turns': turns,
+    'seconds': seconds
+  }
 };
